444505cd536f6e0b629bf6f03a02e30d2706b9e5
Victor <dibiavc@us.ibm.com>
2017-02-14 16:49:28 -0500

    add websocket and log function

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,8 @@
 
     $(".time-label .bg-blue").text(now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear())
 
-    var ws = new WebSocket("ws://localhost:8078");
+    var socketurl = "ws://" + window.location.href.replace(/.*?:\/\//g, "");
+    var ws = new WebSocket(socketurl);
 
     ws.onopen = function() {
         ws.send("Message  from browser ");
